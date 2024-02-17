05e6ffa1215469c57e0a508430bc969ce14207af
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2024-02-17 13:41:09 +0900

    fast exp

--- chapter1/1-3-2/exponentiation.ts
+++ chapter1/1-3-2/exponentiation.ts
@@ -1,27 +1,56 @@
 import { assertEquals } from "https://deno.land/std@0.83.0/testing/asserts.ts";
 
-// iは0より大きい整数
+const exp_for_power_of_2 = (x: number, n: number): number => {
+  if (n === 1) {
+    return x;
+  }
+  while (n > 1) {
+    x = x * x;
+    n = Math.floor(n / 2);
+  }
+  return x;
+};
+
 const exp = (x: number, n: number): number => {
   let y = 1.0;
-
+  let binaryDigitPosition = 1;
   while (n > 0) {
-    console.log(n);
+    console.log(`y = ${y}, x = ${x}, n = ${n}`);
     if (n % 2 !== 0) {
-      y = y * x;
-    } else {
-      (x = x * x), (n = Math.floor(n / 2));
+      console.log(
+        `余りが1なので計算する: x=${x}, binaryDigitPosition=${binaryDigitPosition}`
+      );
+      y = y * exp_for_power_of_2(x, 2 ** (binaryDigitPosition - 1));
     }
+    binaryDigitPosition++;
+    n = Math.floor(n / 2);
   }
+  console.log(`answer = ${y}`);
   return y;
 };
 
-Deno.test("exp", () => {
-  assertEquals(exp(1, 1), 1);
-  assertEquals(exp(1, 2), 1);
+Deno.test("exp_for_power_of_2", () => {
+  assertEquals(exp_for_power_of_2(3, 1), 3);
+  assertEquals(exp_for_power_of_2(3, 2), 9);
+  assertEquals(exp_for_power_of_2(3, 4), 81);
+  assertEquals(exp_for_power_of_2(3, 8), 6561);
+  assertEquals(exp_for_power_of_2(2, 4), 16);
+});
 
-  assertEquals(exp(2, 1), 2);
-  assertEquals(exp(2, 2), 4);
-  assertEquals(exp(2, 3), 8);
+Deno.test("n is power of 2 (n > 0)", () => {
+  assertEquals(exp(3, 1), 3);
+  assertEquals(exp(3, 2), 9);
+  assertEquals(exp(3, 4), 81);
+  assertEquals(exp(3, 8), 6561);
+  assertEquals(exp(2, 4), 16);
+});
 
-  assertEquals(exp(1.5, 2), 2.25);
+Deno.test("n is not power of 2 (n > 0)", () => {
+  assertEquals(exp(3, 3), 27);
+  assertEquals(exp(3, 5), 243);
+  assertEquals(exp(3, 7), 2187);
+});
+
+Deno.test("n is large", () => {
+  assertEquals(exp(2, 30), 1073741824);
 });
