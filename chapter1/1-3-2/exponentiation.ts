0d2feb5c01feb195584f33a9195a4cf9ffd691f6
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2024-02-17 14:08:27 +0900

    refactor

--- chapter1/1-3-2/exponentiation.ts
+++ chapter1/1-3-2/exponentiation.ts
@@ -1,41 +1,20 @@
 import { assertEquals } from "https://deno.land/std@0.83.0/testing/asserts.ts";
 
-const exp_for_power_of_2 = (x: number, n: number): number => {
-  if (n === 1) {
-    return x;
-  }
-  while (n > 1) {
+const exp = (x: number, n: number): number => {
+  // 指数計算のアルゴリズム。xのn乗を計算する。
+  // 指数が2の冪乗の場合、計算量がO(log n)になる。そこで、どのような指数の場合でも、指数を2の累乗の積に分解する。
+  // 例えば、3^5 = (3^4)*1 * (3^2)*0 * (3^1)*1 = 3^4 * 3^1 = 81 * 3 = 243
+  // 以下の実装では、xの（2の累乗）乗を順番に計算しつつ、回答にその数が含まれる場合にはyにその数を掛けていく。
+  let y = 1.0;
+  while (n > 0) {
+    if (n % 2 !== 0) {
+      y = y * x;
+    }
     x = x * x;
     n = Math.floor(n / 2);
   }
-  return x;
-};
-
-const exp = (x: number, n: number): number => {
-  let y = 1.0;
-  let binaryDigitPosition = 1;
-  while (n > 0) {
-    console.log(`y = ${y}, x = ${x}, n = ${n}`);
-    if (n % 2 !== 0) {
-      console.log(
-        `余りが1なので計算する: x=${x}, binaryDigitPosition=${binaryDigitPosition}`
-      );
-      y = y * exp_for_power_of_2(x, 2 ** (binaryDigitPosition - 1));
-    }
-    binaryDigitPosition++;
-    n = Math.floor(n / 2);
-  }
-  console.log(`answer = ${y}`);
   return y;
 };
-
-Deno.test("exp_for_power_of_2", () => {
-  assertEquals(exp_for_power_of_2(3, 1), 3);
-  assertEquals(exp_for_power_of_2(3, 2), 9);
-  assertEquals(exp_for_power_of_2(3, 4), 81);
-  assertEquals(exp_for_power_of_2(3, 8), 6561);
-  assertEquals(exp_for_power_of_2(2, 4), 16);
-});
 
 Deno.test("n is power of 2 (n > 0)", () => {
   assertEquals(exp(3, 1), 3);
