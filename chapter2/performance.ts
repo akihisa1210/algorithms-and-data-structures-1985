d0eede3fce50a32de24d3d38715d9e6ca92218b3
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2024-02-25 11:44:08 +0900

    selection sort

--- chapter2/performance.ts
+++ chapter2/performance.ts
@@ -2,6 +2,7 @@ import { Stopwatch } from "../helpers/stopwatch.ts";
 import { Items } from "../helpers/Items.ts";
 import { insertionSort } from "./insertion-sort.ts";
 import { binaryInsertionSort } from "./binary-insertion-sort.ts";
+import { selectionSort } from "./selection-sort.ts";
 
 // 性能計測用のメソッド
 const measurePerformance = (
@@ -23,3 +24,4 @@ const items = Array.from({ length: 100000 }, () =>
 
 measurePerformance(insertionSort, items);
 measurePerformance(binaryInsertionSort, items);
+measurePerformance(selectionSort, items);
